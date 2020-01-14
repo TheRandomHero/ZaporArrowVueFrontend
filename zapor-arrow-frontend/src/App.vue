80a7c79b715478f3f70303f4ceac42b0650b21de
Attila Molnar <scythianhawk@gmai.com>
2020-01-14 20:10:05 +0100

    setup router

--- zapor-arrow-frontend/src/App.vue
+++ zapor-arrow-frontend/src/App.vue
@@ -1,28 +1,27 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-content>
+      <app-nav-bar />
+      <router-view></router-view>
+    </v-content>
+      <app-footer />
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import NavBar from './components/NavBar'
+  import Footer from './components/Footer'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+  export default {
+    name: 'App',
+    components: {
+      appNavBar: NavBar,
+      appFooter: Footer
+    }
+  };
 </script>
 
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
