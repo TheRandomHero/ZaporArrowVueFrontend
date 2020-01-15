e961f90a1213292242eb52bb7f2453166a900920
Attila Molnar <scythianhawk@gmai.com>
2020-01-15 10:51:20 +0100

    filled with dummy data to test

--- zapor-arrow-frontend/src/views/Gallery.vue
+++ zapor-arrow-frontend/src/views/Gallery.vue
@@ -20,9 +20,36 @@
 </template>
 <script>
 export default {
-    
+    data() {
+            return {
+                arrows:[
+                    {id:'1', imgSrc: require('../../../ZaporArrow/wwwroot/images/084f2d21-584d-4bb5-bc57-1a6f3b276fe1_znb1.jpg'), length: '#30', description:'extra donga 7mm, 22-22,2gramm 30 font!'},
+                    {id:'2', imgSrc:require('../../../ZaporArrow/wwwroot/images/1f5df3e2-400f-499f-9850-19b4116f37b4_szpn1.jpg'), length: '#29', description:'extra donga 7mm, 22-22,2gramm 30 font!'},
+                    {id:'3', imgSrc:require('../../../ZaporArrow/wwwroot/images/2d223d00-c18a-4158-ac78-803117085e9b_bfn.jpg'), length: '#50', description:'50+#, 31-31,5g dongás exclusive traditional 5,5" os natúr tollal!'},
+                    {id:'4', imgSrc:require('../../../ZaporArrow/wwwroot/images/svb1.jpg'), length: '#33', description:'extra donga 7mm, 22-22,2gramm 30 font!'},
+                    {id:'5', imgSrc:require('../../../ZaporArrow/wwwroot/images/b647c749-f46b-4c79-9129-b35a60277ae9_bfb1.jpg'), length: '#42', description:'extra donga 7mm, 22-22,2gramm 30 font!'},
+                    {id:'6', imgSrc:require('../../../ZaporArrow/wwwroot/images/7b2cb54c-0e37-4a4c-a61c-f6efb2eef96f_skn1.jpg'), length: '#22', description:'50+#, 31-31,5g dongás exclusive traditional 5,5" os natúr tollal'},
+                    {id:'7', imgSrc:require('../../../ZaporArrow/wwwroot/images/bfb1.jpg'), length: '#26', description:'extra donga 7mm, 22-22,2gramm 30 font!'},
+                ]
+            }
+        }
 }
 </script>
 <style scoped>
-
+    .gallery {
+            position: relative;
+            background-image: url('../assets/mainbg.png');
+            width: 100%;
+            height: auto;
+            padding-top: 20%;
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: cover;
+        }
+        .main-title{
+            font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sansp
+        }
+        .container{
+        padding: 0px
+    }
 </style>
