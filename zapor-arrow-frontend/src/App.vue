b33e9d615e0fe558e56d5070c649e8e4c88579fb
Attila Molnar <scythianhawk@gmai.com>
2020-01-15 10:22:05 +0100

    images added for dev

--- zapor-arrow-frontend/src/App.vue
+++ zapor-arrow-frontend/src/App.vue
@@ -1,27 +1,28 @@
 <template>
-  <v-app>
-    <v-content>
-      <app-nav-bar />
-      <router-view></router-view>
-    </v-content>
-      <app-footer />
-  </v-app>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
-  import NavBar from './components/NavBar'
-  import Footer from './components/Footer'
+import HelloWorld from './components/HelloWorld.vue'
 
-  export default {
-    name: 'App',
-    components: {
-      appNavBar: NavBar,
-      appFooter: Footer
-    }
-  };
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
 </script>
 
-
 <style>
-
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
