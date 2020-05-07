3e835297bd37301c1a61b9b9f88d116c08848ce6
Attila Molnar <scythianhawk@gmail.com>
2020-05-07 13:12:08 +0200

    DefinePlugin added to vue.config.js

--- zapor-arrow-frontend/vue.config.js
+++ zapor-arrow-frontend/vue.config.js
@@ -2,8 +2,12 @@
 module.exports = {
     configureWebpack: config => {
         if (process.env.NODE_ENV === 'production') {
-            process.env.VUE_APP_ROOT_API = 'https://zaporarrowapi.azurewebsites.net'
-        } 
+            new webpack.DefinePlugin({
+                'process.env': {
+                  VUE_APP_ROOT_API: 'https://zaporarrowapi.azurewebsites.net'
+                }
+        })
+        }
         else 
         {
             devServer:{
