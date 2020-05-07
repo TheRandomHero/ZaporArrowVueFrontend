c53da43c0f5656dfd17d4665dbf4d2414c856728
Attila Molnar <scythianhawk@gmail.com>
2020-05-07 15:21:59 +0200

    removed env files from gitignore

--- zapor-arrow-frontend/vue.config.js
+++ zapor-arrow-frontend/vue.config.js
@@ -1,22 +1,8 @@
-const webpack = require('webpack');
 
 
 module.exports = {
-    configureWebpack: config => {
-        if (process.env.NODE_ENV === 'production') {
-            plugins:[
-                new webpack.DefinePlugin({
-                    'process.env': {
-                      VUE_APP_ROOT_API: JSON.stringify("https://zaporarrowapi.azurewebsites.net")
-                    }
-            })
-            ]
-        }
-        else 
-        {
-            devServer:{
-                host: 'localhost'
-            }
-        }
-      }
+    devServer:{
+        host: 'localhost'
+    }
+        
 };
