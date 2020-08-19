c3f8445f200b2ff3fa7a458f49a45ae1e60b69f3
unknown <scythianhawk@gmail.com>
2020-08-19 18:21:10 +0200

    setting up authentication to firebase

--- zapor-arrow-frontend/src/main.js
+++ zapor-arrow-frontend/src/main.js
@@ -5,6 +5,7 @@ import vuetify from './plugins/vuetify';
 import VueResource from 'vue-resource';
 import cookies from 'vue-cookies';
 import Cloudinary, {CldImage, CldTransformation} from 'cloudinary-vue';
+import * as firebase from 'firebase';
 
 import '@fortawesome/fontawesome-free/css/all.css'
 
@@ -12,6 +13,7 @@ import { store } from './store/store';
 
 
 Vue.config.productionTip = false
+
 Vue.use(VueResource);
 Vue.use(cookies);
 Vue.use(Cloudinary, {
@@ -28,6 +30,18 @@ Vue.use(Cloudinary, {
 } 
 });
 
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_FIREBASEAPIKEY,
+  authDomain: "zaporarrow-7f6ed.firebaseapp.com",
+  databaseURL: "https://zaporarrow-7f6ed.firebaseio.com",
+  projectId: "zaporarrow-7f6ed",
+  storageBucket: "zaporarrow-7f6ed.appspot.com",
+  messagingSenderId: process.env.VUE_APP_FIREBASEMESSAGESENDERID,
+  appId: process.env.VUE_APP_FIREBASEAPPID,
+  measurementId: process.env.VUE_APP_FIREBASEMEASUREMENTID
+};
+
+firebase.initializeApp(firebaseConfig);
 
 Vue.use(vuetify, {
   iconfont: 'fa'
